856ad2e97db81c9121f915fe79774b96c5beeeb0
Fatima MOUMNI <fatima.moumni@hotmail.fr>
2022-01-31 12:06:24 +0100

    add model classes

---
+++ src/main/java/com/safetynet/alerts/model/FireStation.java
@@ -0,0 +1,10 @@
+package com.safetynet.alerts.model;
+
+import lombok.Data;
+
+@Data
+public class FireStation {
+	private String address;
+	private int station;
+
+}
---
+++ src/main/java/com/safetynet/alerts/model/MedicalRecord.java
@@ -0,0 +1,17 @@
+package com.safetynet.alerts.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MedicalRecord {
+
+	private String firstName;
+	private String lastName;
+
+	private String birthdate;
+	private List<String> medications;
+	private List<String> allergies;
+
+}
